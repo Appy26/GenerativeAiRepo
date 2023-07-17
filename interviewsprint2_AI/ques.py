a93d0d6eb55656bc7694b2bd87eddae17cc441a7
Appy26 <aparnaharsh2604@gmail.com>
2023-07-17 17:50:08 +0530

    interviewSprint2Round2

---
+++ interviewsprint2_AI/ques.py
@@ -0,0 +1,12 @@
+# problem no4
+def is_palindrome_number(number):
+   
+    number_str = str(number)
+    
+    
+    if number_str == number_str[::-1]:
+        return True
+    else:
+        return False
+    is_palindrome_number()
+   
